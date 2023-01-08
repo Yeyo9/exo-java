feecd7cabfdc6cf9105f59bb915a455da407ca16
youssef <kingyou5599@gmail.com>
2023-01-08 02:00:20 +0100

    exo

--- index2.js
+++ index2.js
@@ -1,3 +1,29 @@
+/*# Définition de variables*/
 let nom = "John";
 console.log(nom)
-/* sur le terminal, c'est affiché John*/ 
+/*sur le terminal, c'est affiché John*/
+nom = "Jane";
+console.log(nom)
+
+/*# Boucles*/
+
+for(let= i=1;i<=10;i++)
+{
+    console.log(i)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
